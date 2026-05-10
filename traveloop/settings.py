fcbafc6227f79c7c9b929eee785655c14f655c60
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 18:45:05 +0530

    Update settings.py

--- traveloop/settings.py
+++ traveloop/settings.py
@@ -52,12 +52,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "traveloop.wsgi.application"
 
+import os
+import dj_database_url
+
+# Database configuration
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# This part tells Django to use the Render database when deployed
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
