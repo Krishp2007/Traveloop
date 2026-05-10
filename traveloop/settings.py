d3cfbd32fe101963d8bf742c57ca88314ea0266e
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 16:59:59 +0530

    Update settings.py

--- traveloop/settings.py
+++ traveloop/settings.py
@@ -91,7 +91,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'shleshdarji317@gmail.com'
-EMAIL_HOST_PASSWORD = 'hucf wzvi ilsv gytz'
+EMAIL_HOST_USER = 'yourmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your pass'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
